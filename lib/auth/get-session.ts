ff6136148caaae6a299c53a544f8e66561fdcf6d
Khurram Shafique <kshafique40@gmail.com>
2026-07-14 20:38:47 +0500

    deleting extra

--- lib/auth/get-session.ts
+++
@@ -1,8 +0,0 @@
-import { cookies } from "next/headers";
-import { SESSION_COOKIE, verifySessionToken } from "@/lib/auth/token";
-
-export async function getSession() {
-  const store = await cookies();
-  const token = store.get(SESSION_COOKIE)?.value;
-  return verifySessionToken(token);
-}
